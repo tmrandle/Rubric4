0afb41d2484d274af97849d54421d3969a69410d
tmrandle <60156426+tmrandle@users.noreply.github.com>
2021-03-05 14:47:07 -0600

    working on eventlistheners and css

--- js/myScript.js
+++ js/myScript.js
@@ -1,13 +1,28 @@
+var body = document.getElementById("page");
 var numOne = document.getElementById("numOne");
 var numTwo = document.getElementById("numTwo");
 var addSum = document.getElementById("sum");
-var Wren = document.getElementById("wren");
+var wren = document.getElementById("wren");
+var bBird = document.getElementById("bBird");
 var navBar;
 var liTag;
 var liLinks;
+var date = new Date();
+var weekDate = date.getDay();
 
-numOne.addEventListener("input", collectAndAdd);
-numTwo.addEventListener("input" , collectAndAdd);
+
+function checkCookies() {
+    var text = '';
+
+    if (navigator.cookieEnabled == true) {
+        text = "Cookies are enabled.";
+    }else{
+        text = "Cookies are not enabled";
+    }
+    console.log(text);
+}
+
+body.addEventListener("load",checkCookies(Event));
 
 function collectAndAdd() {
     let one = parseInt(numOne.value) || 0;
@@ -16,7 +31,9 @@ function collectAndAdd() {
     addSum.innerHTML = "Your sum is " + sum;
 }
 
-wren.addEventListener("click", showPic);
+
+numOne.addEventListener("input", collectAndAdd);
+numTwo.addEventListener("input" , collectAndAdd);
 
 function showPic() {
     //to hide all the images on the page
@@ -39,20 +56,23 @@ function showPic() {
     
 }
 
+wren.addEventListener("click", showPic);
+bBird.addEventListener("click", showPic);
+
 //working with getElementById element in an variable named navBar
 navBar = document.getElementById("navBar");
 console.log(navBar.childNodes.length);
 navBar.setAttribute("align", "center");
 
 liTag = document.getElementsByTagName("li");
-console.log("Number of li links in navBar " + liTag.length); 
+console.log("Number of li links in menuItem " + liTag.length); 
 
-//working with getElementsByTagName
+working with getElementsByTagName
 liLinks = navBar.getElementsByTagName("a");
 console.log("Text of li link index 0 " + liLinks[0].text);
 for (let i = 0; i < liLinks.length; i++){
 liLinks[i].setAttribute("style", "color: blue");
-}
+ }
 
 navList.addEventListener("mouseover", function(event){
     //highlights the mouseover target red on mouseover
@@ -62,3 +82,6 @@ navList.addEventListener("mouseover", function(event){
         event.target.style.color ="blue";
     }, 700);
 },false);
+
+//this displays the date on the page in the p with id="time"
+document.getElementById("time").innerHTML = "Today is " + (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear();
